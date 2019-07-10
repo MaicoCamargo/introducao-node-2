797198e3aad89f7c5f887b8f120460b9d23d5e2b
Maico Camargo <maico.silva@meta.com.br>
2019-07-10 17:39:43 -0300

    salvando edicao dos dados no banco

--- routers/admin.js
+++ routers/admin.js
@@ -11,22 +11,22 @@ const Categoria = mongoose.model('categorias'); // criar referencia da collectio
 /**
  * carrega a tela inicial do admin
  */
-router.get('/', (request,response) =>{
+router.get('/', (request, response) => {
     response.send('home');
 });
 
-router.get('/posts', (request,response) =>{
+router.get('/posts', (request, response) => {
     response.send('home');
 });
 
 /**
  * carrega a pag com a lista das categorias
  */
-router.get('/categorias', (request,response) =>{
-    Categoria.find().sort({data:'desc'})
-        .then( (categorias) => response.render('admin/categorias-list',{categorias: categorias}))
-        .catch( () => {
-            request.flash('error_msg','erro no listar as categorias');
+router.get('/categorias', (request, response) => {
+    Categoria.find().sort({data: 'desc'})
+        .then((categorias) => response.render('admin/categorias-list', {categorias: categorias}))
+        .catch(() => {
+            request.flash('error_msg', 'erro no listar as categorias');
             response.render('/categorias');
         });
 
@@ -35,39 +35,39 @@ router.get('/categorias', (request,response) =>{
 /**
  * carrega o form de add categorias
  */
-router.get('/categorias/adicionar-categoria', (request,response) =>{
+router.get('/categorias/adicionar-categoria', (request, response) => {
     response.render('admin/form-categoria');
 });
 
 /**
  * salva uma nova categoria no banco
  */
-router.post('/categorias/adicionar-categoria', (request,response) =>{
+router.post('/categorias/adicionar-categoria', (request, response) => {
 
-    let erros =[];
-    if(!request.body.nome || request.body.nome < 2){
-        erros.push({ texto: 'nome invalido, insira um nome valido'})
+    let erros = [];
+    if (!request.body.nome || request.body.nome < 2) {
+        erros.push({texto: 'nome invalido, insira um nome valido'})
     }
-    if (!request.body.slug || request.body.slug < 2){
+    if (!request.body.slug || request.body.slug < 2) {
         erros.push({texto: 'slug invalido, insira um slug valido'})
     }
 
-    if (erros.length > 0) { response.render('admin/form-categoria', {erros: erros});}
-
-    else {
+    if (erros.length > 0) {
+        response.render('admin/form-categoria', {erros: erros});
+    } else {
         const nova = ({
             nome: request.body.nome,
             slug: request.body.slug,
         });
 
         new Categoria(nova).save()
-            .then( ()=> {
-                request.flash('success_msg','categoria cadastrada com sucesso');
+            .then(() => {
+                request.flash('success_msg', 'categoria cadastrada com sucesso');
                 response.redirect('/admin/categorias');
 
             })
-            .catch( (err) => {
-                request.flash('error_msg','ocorreu um erro no cadastrar categoria, tente novamente');
+            .catch((err) => {
+                request.flash('error_msg', 'ocorreu um erro no cadastrar categoria, tente novamente');
                 response.redirect('/admin/categorias');
 
             });
@@ -79,20 +79,49 @@ router.post('/categorias/adicionar-categoria', (request,response) =>{
 /**
  * carregar a pagina com form para editar categoria
  */
-router.get('/categorias/editar-categoria/:id', (request,response) => {
+router.get('/categorias/editar-categoria/:id', (request, response) => {
 
     Categoria.findOne({_id: request.params.id})
-        .then( (resultado) => {
-            console.log(resultado.nome);
-            console.log(resultado.slug);
-            response.render('admin/categoria-edit',{categoria: resultado});
-        })
-        .catch( () => {
+        .then((resultado) => {
 
-            request.flash('error_msg','está categoria não foi encontrada');
-            response.redirect('admin/categorias');
+            response.render('admin/categoria-edit', {categoria: resultado});
+        })
+        .catch(() => {
+
+            request.flash('error_msg', 'está categoria não foi encontrada');
+            response.redirect('/admin/categorias');
         });
 
 
 });
+
+router.post('/categorias/editar-categoria', (request, response) => {
+    console.log(request.body);
+    Categoria.findOne({id: request.body.id})
+        .then((resultado) => {
+
+            resultado.nome = request.body.nome;
+            resultado.slug = request.body.slug;
+            resultado.data = Date.now();
+
+            resultado.save()
+                .then(() => {
+
+                    request.flash('success_msg', 'categoria editada com sucesso');
+                    response.redirect('/admin/categorias');
+                })
+                .catch(() => {
+                    console.log('erro save:' + err);
+
+                    request.flash('error_msg', 'erro no editar categoria');
+                    response.redirect('/admin/categorias');
+                });
+        })
+        .catch((err) => {
+            console.log('erro findOne:' + err);
+            request.flash('error_msg', 'erro no editar categoria');
+            response.redirect('/admin/categorias');
+        });
+});
+
 module.exports = router;
